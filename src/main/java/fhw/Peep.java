56f6cd20db71282fdae711b08f8c1fe92ae15d66
fwelland <fwelland@weedeater.promontory-hq.promcapital.com>
2015-10-22 08:02:46 -0400

    init-rev

---
+++ src/main/java/fhw/Peep.java
@@ -0,0 +1,39 @@
+package fhw;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.validation.constraints.NotNull;
+import lombok.Data;
+
+@Named
+@Data
+@RequestScoped
+public class Peep
+{
+    public Peep()
+    {
+        
+    }
+
+    @PostConstruct
+    private void init()
+    {
+        System.out.println("I got structed...");
+    }
+    
+    
+    
+    
+    @NotNull(message = "you gotta have a name")
+    private String name; 
+    
+    @NotNull(message = "you gotta have a title")
+    private String title; 
+    
+    
+    public void go()
+    {
+        System.out.println( " go pushed; name [" + name + "]; title [" + title + "]");
+    }
+}
